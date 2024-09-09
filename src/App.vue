194af24e8366042e98b8f4e108975203a97fcd4c
estebanllfu <estebanllfu@gmail.com>
2024-09-09 11:24:24 -0300

    Vinculamos componentes y sanitización

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
-<script>
-
-
-</script>
-
-
-
 <template>
- <div>
-  
- </div>
+  <div id="#app">
+    <h1>Lugares que quiero visitar</h1>
+    <PlaceForm @add-place="handleAddPlace" />
+    <ul>
+      <li v-for="place in placesToVisit" :key="place">
+        {{ place }}
+      </li>
+    </ul>
+  </div>
 </template>
-
-
-<style>
-
-</style>
+ 
+<script>
+import PlaceForm from "./components/PlaceForm.vue";
+ 
+export default {
+  components: {
+    PlaceForm,
+  },
+ 
+  data() {
+    return {
+      placesToVisit: [],
+    };
+  },
+ 
+  methods: {
+    handleAddPlace(place) {
+      if (place && !this.placesToVisit.includes(place)) {
+        this.placesToVisit.push(place);
+      }
+    },
+  },
+};
+</script>
--- src/components/PlaceForm.vue
+++ src/components/PlaceForm.vue
@@ -2,9 +2,13 @@
     <div>
         <label for="place-select">Selecciona un lugar</label>
         <select v-model="selectedPlace" id="place-select">
-            <option v-for="place in places" :key="place.name" :value="place.name">
-                {{place.name}}
+
+            <option v-for="place in places" :key="place.cca3" :value="place.name.common">
+
+                {{ place.name.common }}
+
             </option>
+
         </select>
         <button @click="addPlace">Añadir un lugar</button>
     </div>
